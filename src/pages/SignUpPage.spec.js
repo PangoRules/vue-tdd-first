f702feda79b5e7a3eea64b31c636692993f2b3d0
Rafael Bolaños <animasdelmundo2@gmail.com>
2022-06-06 15:55:00 -0500

    Setup completed

---
+++ src/pages/SignUpPage.spec.js
@@ -0,0 +1,11 @@
+import SignUpPage from "./SignUpPage.vue";
+import {render, screen}  from "@testing-library/vue";
+import "@testing-library/jest-dom";
+
+
+test('Has sign up header',() => {
+    render(SignUpPage);
+    const header = screen.queryByRole('heading', { name: 'Sign Up' });
+
+    expect(header).toBeInTheDocument();
+});
